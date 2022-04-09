9a924a1b249420b8355ac90e3058a4586eb6ae4d
Emil <emil.berlin@arcor.de>
2022-04-09 15:31:19 +0200

    neue Datei:     .gitignore 	geändert:       README.md 	neue Datei:     package-lock.json 	neue Datei:     package.json 	neue Datei:     public/apple-touch-icon-precomposed.png 	neue Datei:     public/ewd-logo.png 	neue Datei:     public/favicon.ico 	neue Datei:     public/index.html 	neue Datei:     public/manifest.json 	neue Datei:     public/robots.txt 	neue Datei:     src/App.js 	neue Datei:     src/index.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <header className="uk-section uk-section-default uk-flex uk-flex-middle uk-flex-between uk-padding-small">
+      <h1 className="uk-h4 uk-margin-remove-vertical uk-padding">
+        <img src="/ewd-logo.png" alt="logo" />
+        <span>React-Strapi Blog</span>
+      </h1>
+    </header>
+  )
+}
+
+export default App
